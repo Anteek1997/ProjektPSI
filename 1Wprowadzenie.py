b6d83135502f31dde5b4a0229e8afee03abb5aa2
Matii123 <mateuszbialek941@gmail.com>
2020-10-26 12:25:43 +0100

    Zadanie 1 i 2

---
+++ 1Wprowadzenie.py
@@ -0,0 +1,12 @@
+print("Zadanie1")
+tekst = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker"
+print(tekst)
+print("Zadanie2")
+imie = "Mateusz"
+nazwisko = "Białek"
+litera_1 = imie[2]
+litera_2 = nazwisko[3]
+tekst2 = "W tekście jest liter ... oraz liter ..."
+liczba_liter1 = tekst2.count(litera_1)
+liczba_liter2 = tekst2.count(litera_2)
+print('W tekście jest {0} liter {1} oraz {2} liter {3}'.format(liczba_liter1, litera_1, liczba_liter2, litera_2))
