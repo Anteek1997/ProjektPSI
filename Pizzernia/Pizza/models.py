76618e13e4c94573d3c28140d67353f63125c065
Anteek1997 <bartosz.antkiewicz1997@gmail.com>
2020-11-25 23:23:22 +0100

    django

---
+++ Pizzernia/Pizza/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Pizza(models.Model):
+
+  nazwa=models.CharField(max_length=50)
+  opis = models.TextField(blank=True)
+  cena = models.DecimalField(max_digits=8, decimal_places=2)
+
