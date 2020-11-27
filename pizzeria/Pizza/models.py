50b30389c3f992e20b8a4c0bf763dd21557e7ec5
Matii123 <mateuszbialek941@gmail.com>
2020-11-27 21:16:23 +0100

    tabele Sos i Pizza

--- pizzeria/Pizza/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Pizza(models.Model):
-    nazwa = models.CharField(max_length=50)
-    opis =  models.TextField(blank=True)
-    cena =  models.DecimalField(max_digits=5,decimal_places=2)
-
