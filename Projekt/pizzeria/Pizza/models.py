0fd43b8312724216d3ef6a58451efb802853aff3
Matii123 <mateuszbialek941@gmail.com>
2020-11-27 21:27:03 +0100

    Usuniete

--- Projekt/pizzeria/Pizza/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Pizza(models.Model):
-    nazwa = models.CharField(max_length=50)
-    opis =  models.TextField(blank=True)
-    cena =  models.DecimalField(max_digits=5,decimal_places=2)
-
-    def __str__(self):
-        return self.nazwa + ' ' + str(self.cena) + 'zl'
--- Projekt/pizzeria/Zamowienia/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ZamowieniaConfig(AppConfig):
-    name = 'Zamowienia'
